42d95bc864c6e7afd8e3c0ddb091830541c25ffa
pavladan <fatalfenker@gmail.com>
2019-11-01 10:28:03 +0300

    Add youtube auto transcript support

--- index.js
+++ index.js
@@ -12,25 +12,35 @@ var parser = (function() {
      *
      * @param  {String}  data SubRip suntitles string
      * @param  {Boolean} ms   Optional: use milliseconds for startTime and endTime
-     * @return {Array}  
+     * @return {Array}
      */
-    pItems.fromSrt = function(data, ms) {
+    pItems.fromSrt = function(data, ms, isYoutubeAutoTranscript) {
         var useMs = ms ? true : false;
-
-        data = data.replace(/\r/g, '');
+        var useYoutubeAutoTranscript = isYoutubeAutoTranscript ? true : false;
+        data = data.replace(/\r/g, "");
         var regex = /(\d+)?\n?(\d{2}:\d{2}:\d{2}[,.]\d{3}) --> (\d{2}:\d{2}:\d{2}[,.]\d{3}).*\n/g;
         data = data.split(regex);
         data.shift();
         var items = [];
         for (var i = 0; i < data.length; i += 4) {
+            var text = data[i + 3];
+            if (useYoutubeAutoTranscript) {
+                text = text.split("\n");
+                text.shift();
+                text = text.join("\n");
+            }
+            if (text.trim().length === 0) continue;
             items.push({
-                id: data[i] ? data[i].trim() : (items.length+1).toString(),
-                startTime: useMs ? timeMs(data[i + 1].trim()) : data[i + 1].trim(),
-                endTime: useMs ? timeMs(data[i + 2].trim()) : data[i + 2].trim(),
-                text: data[i + 3].trim()
+                id: data[i] ? data[i].trim() : (items.length + 1).toString(),
+                startTime: useMs
+                    ? timeMs(data[i + 1].trim())
+                    : data[i + 1].trim(),
+                endTime: useMs
+                    ? timeMs(data[i + 2].trim())
+                    : data[i + 2].trim(),
+                text: text.trim()
             });
         }
-
         return items;
     };
 
@@ -40,8 +50,8 @@ var parser = (function() {
      * @return {String}      SubRip subtitles string
      */
     pItems.toSrt = function(data) {
-        if (!data instanceof Array) return '';
-        var res = '';
+        if (!data instanceof Array) return "";
+        var res = "";
 
         for (var i = 0; i < data.length; i++) {
             var s = data[i];
@@ -51,9 +61,9 @@ var parser = (function() {
                 s.endTime = msTime(parseInt(s.endTime, 10));
             }
 
-            res += s.id + '\r\n';
-            res += s.startTime + ' --> ' + s.endTime + '\r\n';
-            res += s.text.replace('\n', '\r\n') + '\r\n\r\n';
+            res += s.id + "\r\n";
+            res += s.startTime + " --> " + s.endTime + "\r\n";
+            res += s.text.replace("\n", "\r\n") + "\r\n\r\n";
         }
 
         return res;
@@ -73,33 +83,35 @@ var parser = (function() {
         }
 
         // hours + minutes + seconds + ms
-        return parts[1] * 3600000 + parts[2] * 60000 + parts[3] * 1000 + parts[4];
+        return (
+            parts[1] * 3600000 + parts[2] * 60000 + parts[3] * 1000 + parts[4]
+        );
     };
 
     var msTime = function(val) {
-        var measures = [ 3600000, 60000, 1000 ]; 
+        var measures = [3600000, 60000, 1000];
         var time = [];
 
         for (var i in measures) {
-            var res = (val / measures[i] >> 0).toString();
-            
-            if (res.length < 2) res = '0' + res;
+            var res = ((val / measures[i]) >> 0).toString();
+
+            if (res.length < 2) res = "0" + res;
             val %= measures[i];
             time.push(res);
         }
 
         var ms = val.toString();
         if (ms.length < 3) {
-            for (i = 0; i <= 3 - ms.length; i++) ms = '0' + ms;
+            for (i = 0; i <= 3 - ms.length; i++) ms = "0" + ms;
         }
 
-        return time.join(':') + ',' + ms;
+        return time.join(":") + "," + ms;
     };
 
     return pItems;
 })();
 
 // ignore exports for browser
-if (typeof exports === 'object') {
+if (typeof exports === "object") {
     module.exports = parser;
 }
